e7e6cee65e8a4e931cb714ca0295e483814ce100
Juan Cisneros <juanfrancistm2011@icloud.com>
2022-08-24 21:28:53 -0500

    MAIN MENU AND SETTINGS MENU DESIGN FINISHED

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,8 +13,46 @@ class HomeScreen extends StatelessWidget{
       ),
       drawer: SideMenuWidget(), //Drawer is a widget that is used to display a navigation menu.
       body: Center(
-        child: Text('HomeScreen'),
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _Content(title: 'Dark Mode', state : ''),
+              SizedBox(height: 20),
+              _Content(title: 'Gender', state : ''),
+              SizedBox(height: 20),
+              _Content(title: 'User Name', state : ''),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
+
+class _Content extends StatelessWidget {
+
+  final String title;
+  final String state;
+
+  const _Content({
+    Key? key, required this.title, required this.state,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      //ROUND CORNERS
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
       ),
+      elevation: 5,
+      child:Container(
+        padding: EdgeInsets.all(20),
+          child: Text('${this.title} : ${this.state}', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),)
+      ),
+
     );
   }
 }
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 import '../widgets/widgets.dart';
@@ -12,9 +13,82 @@ class SettingsScreen extends StatelessWidget{
         title: Text('SettingsScreen'),
       ),
       drawer: SideMenuWidget(), //Drawer is a widget that is used to display a navigation menu.
-      body: Center(
-        child: Text('SettingsScreen'),
-      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+
+              Card(
+                //ROUND CORNERS
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                //add elevation to give it a 3D effect
+                elevation: 5,
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('SETTINGS', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),),
+                      SizedBox(height: 40),
+                      Text('Theme', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+                      SwitchListTile.adaptive(
+                        title: Text('Dark Mode', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                        value: true,
+                        onChanged: (value) {},
+                      ),
+                      SizedBox(width: 60),
+                      Text('Gender', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+                      SizedBox(width: 60),
+                      RadioListTile<int>(
+                          value: 1,
+                          groupValue: 1,
+                          title: const Text('Masculine', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                          onChanged: (value){},
+                      ),
+
+                      SizedBox(width: 20),
+                      RadioListTile<int>(
+                        value: 2,
+                        groupValue: 1,
+                        title: const Text('Feminine', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                        onChanged: (value){},
+                      ),
+
+                      SizedBox(width: 20),
+                      RadioListTile<int>(
+                        value: 3,
+                        groupValue: 1,
+                        title: const Text('Other', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                        onChanged: (value){},
+                      ),
+
+                      SizedBox(height: 40),
+                      Text('Name', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+                      SizedBox(width: 20),
+
+                      TextFormField(
+                        initialValue: "",
+                        decoration: InputDecoration(
+                          labelText: 'Name',
+                          helperText: 'Enter your name',
+                          ),
+                        ),
+
+
+
+                    ],
+                  ),
+                ) ,
+
+              )
+
+            ],
+          ),
+        ),
+      )
     );
   }
 }
