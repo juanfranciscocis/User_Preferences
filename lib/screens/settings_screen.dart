6c71f0f6d8966afa5f26317bb2c1930109396366
Juan Cisneros <juanfrancistm2011@icloud.com>
2022-08-24 21:35:58 -0500

    MAIN MENU AND SETTINGS MENU DESIGN FINISHED AND WORKING

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -3,8 +3,19 @@ import 'package:flutter/material.dart';
 
 import '../widgets/widgets.dart';
 
-class SettingsScreen extends StatelessWidget{
+class SettingsScreen extends StatefulWidget{
   const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+
+  bool isDarkmode = false;
+  int gender = 1;
+  String name = '';
+
 
   @override
   Widget build(BuildContext context) {
@@ -14,79 +25,96 @@ class SettingsScreen extends StatelessWidget{
       ),
       drawer: SideMenuWidget(), //Drawer is a widget that is used to display a navigation menu.
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
 
-              Card(
-                //ROUND CORNERS
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                //add elevation to give it a 3D effect
-                elevation: 5,
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text('SETTINGS', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),),
-                      SizedBox(height: 40),
-                      Text('Theme', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
-                      SwitchListTile.adaptive(
-                        title: Text('Dark Mode', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                        value: true,
-                        onChanged: (value) {},
-                      ),
-                      SizedBox(width: 60),
-                      Text('Gender', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
-                      SizedBox(width: 60),
-                      RadioListTile<int>(
-                          value: 1,
-                          groupValue: 1,
-                          title: const Text('Masculine', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                          onChanged: (value){},
-                      ),
+            Card(
+              //ROUND CORNERS
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              //add elevation to give it a 3D effect
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 30),
+                    Text('SETTINGS', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),),
+                    SizedBox(height: 40),
+                    Text('Theme', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+                    SwitchListTile.adaptive(
+                      title: Text('Dark Mode', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                      value: isDarkmode,
+                      onChanged: (value) {
+                        isDarkmode = value;
+                        setState(() {});
+                      },
+                    ),
+                    SizedBox(width: 60),
+                    Text('Gender', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+                    SizedBox(width: 60),
+                    RadioListTile<int>(
+                        value: 1,
+                        groupValue: gender,
+                        title: const Text('Masculine', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                        onChanged: (value){
+                          gender = value??1;
+                          setState(() {});
+                        },
+                    ),
 
-                      SizedBox(width: 20),
-                      RadioListTile<int>(
-                        value: 2,
-                        groupValue: 1,
-                        title: const Text('Feminine', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                        onChanged: (value){},
-                      ),
+                    SizedBox(width: 20),
+                    RadioListTile<int>(
+                      value: 2,
+                      groupValue: gender,
+                      title: const Text('Feminine', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                      onChanged: (value){
+                        gender = value??2;
+                        setState(() {});
+                      },
+                    ),
 
-                      SizedBox(width: 20),
-                      RadioListTile<int>(
-                        value: 3,
-                        groupValue: 1,
-                        title: const Text('Other', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                        onChanged: (value){},
-                      ),
+                    SizedBox(width: 20),
+                    RadioListTile<int>(
+                      value: 3,
+                      groupValue: gender,
+                      title: const Text('Other', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                      onChanged: (value){
+                        gender = value??3;
+                        setState(() {});
+                      },
+                    ),
 
-                      SizedBox(height: 40),
-                      Text('Name', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
-                      SizedBox(width: 20),
+                    SizedBox(height: 40),
+                    Text('Name', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+                    SizedBox(width: 20),
 
-                      TextFormField(
-                        initialValue: "",
-                        decoration: InputDecoration(
-                          labelText: 'Name',
-                          helperText: 'Enter your name',
-                          ),
+                    TextFormField(
+                      initialValue: "",
+                      onChanged: (value){
+                        name = value;
+                        setState(() {});
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Name',
+                        helperText: 'Enter your name',
                         ),
+                      ),
+                    SizedBox(height: 30),
 
 
 
-                    ],
-                  ),
-                ) ,
+                  ],
+                ),
+              ) ,
 
-              )
+            )
 
-            ],
-          ),
+          ],
         ),
       )
     );
